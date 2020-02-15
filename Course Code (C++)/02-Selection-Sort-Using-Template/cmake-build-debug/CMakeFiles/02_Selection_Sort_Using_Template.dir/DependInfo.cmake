4d02ee07ee4c1c9f6a1792ac61f1153d0ac2f46b
martin.wang <martin.wang@cootek.com>
2020-02-15 10:30:20 +0800

    基础排序

---
+++ Course Code (C++)/02-Selection-Sort-Using-Template/cmake-build-debug/CMakeFiles/02_Selection_Sort_Using_Template.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wangchenlong/Documents/mooc/基础部分/algorithm/coding-71/02-Sorting-Basic/Course Code (C++)/02-Selection-Sort-Using-Template/main.cpp" "/Users/wangchenlong/Documents/mooc/基础部分/algorithm/coding-71/02-Sorting-Basic/Course Code (C++)/02-Selection-Sort-Using-Template/cmake-build-debug/CMakeFiles/02_Selection_Sort_Using_Template.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Course Code (C++)/02-Selection-Sort-Using-Template/cmake-build-debug/CMakeFiles/02_Selection_Sort_Using_Template.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02_Selection_Sort_Using_Template"
+  "02_Selection_Sort_Using_Template.pdb"
+  "CMakeFiles/02_Selection_Sort_Using_Template.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_Selection_Sort_Using_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
